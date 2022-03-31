51136b18836bbec08baa9900879a920997bec072
Pawwol1 <wolski.pawel1@gmail.com>
2022-03-31 16:49:43 +0200

    added config files

--- charity-app/src/App.js
+++ charity-app/src/App.js
@@ -1,25 +1,17 @@
-import logo from './assets/logo.svg';
-import './scss/App.css';
+import React from 'react'
+// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Home from './components/Home'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+const App = () => {
+    return (
+        // <Router>
+            <div className="App">
+                    <Home/>
+            </div>
+        // </Router>
+    );
 }
 
 export default App;
--- charity-app/src/index.js
+++ charity-app/src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './scss/index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import './main.scss';
+import 'normalize.css'
 
 ReactDOM.render(
   <React.StrictMode>
