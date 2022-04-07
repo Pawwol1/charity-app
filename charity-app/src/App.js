1dcb2d3f879b351e493d22e29c2ad8e4b3966503
Pawwol1 <wolski.pawel1@gmail.com>
2022-04-07 15:57:52 +0200

    added header

--- charity-app/src/App.js
+++ charity-app/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './components/Home'
+import Header from "./components/Header";
 
 
 
@@ -8,6 +9,7 @@ const App = () => {
     return (
         <Router>
             <div className="App">
+                <Header/>
                 <Routes>
                     <Route path="/" element={<Home />}/>
                 </Routes>
