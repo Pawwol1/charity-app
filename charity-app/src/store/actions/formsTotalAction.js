395c886597c071327e3016383f189f09b09f5485
Paweł Wolski <96190250+Pawwol1@users.noreply.github.com>
2022-04-26 12:15:10 +0200

    Delete charity-app directory

--- charity-app/src/store/actions/formsTotalAction.js
+++
@@ -1,8 +0,0 @@
-export function fetchFormsData() {
-    return function(dispatch) {
-        return fetch(`https://db-oddaj-rzeczy.herokuapp.com/forms`)
-                .then((resp) => resp.json())
-                .then((json)=> dispatch({type: 'FETCH_FORMS_SUCCESS', payload: json}))
-                .catch(error => dispatch({type:'FETCH_FORMS_ERROR', error}) )
-    }
-}
--- charity-app/src/store/actions/fundAction.js
+++
@@ -1,8 +0,0 @@
-export function fetchFundData() {
-    return function(dispatch) {
-        return fetch(`https://db-oddaj-rzeczy.herokuapp.com/foundations`)
-                .then((resp) => resp.json())
-                .then((json)=> dispatch({type: 'FETCH_FUNDS_SUCCESS', payload: json}))
-                .catch(error => dispatch({type:'FETCH_FUNDS_ERROR', error}) )
-    }
-}
